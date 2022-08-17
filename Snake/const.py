9938b2f58a4e6c2875c4eccdc45b79b3be422743
BlackGn0m <47257286+BlackGn0m@users.noreply.github.com>
2022-08-17 11:08:54 +0300

    Changes in PlayingField class Fix Const class

--- Snake/const.py
+++ Snake/const.py
@@ -1,10 +1,18 @@
 class Const:
     def __init__(self):
-        self._debug = True
+        self._debug = False
         self._basewidth = 1024
         self._baseheight = 768
         self._fps = 60
+
+        # Colors
         self._bgcolor = (100, 200, 100)
+        self._bordercolor = (50, 100, 50)
+
+        # Ratios
+        self._marginratio = 0.017
+        self._menuratio = 0.25
+        self._fieldratio = 0.75
         pass
 
     @property
@@ -20,9 +28,25 @@ class Const:
         return self._bgcolor
 
     @property
+    def BORDERCOLOR(self):
+        return self._bordercolor
+
+    @property
     def BASEWIDTH(self):
         return self._basewidth
 
     @property
     def BASEHEIGHT(self):
         return self._baseheight
+
+    @property
+    def MARGINRATIO(self):
+        return self._marginratio
+
+    @property
+    def MENURATIO(self):
+        return self._menuratio
+
+    @property
+    def FIELDRATIO(self):
+        return self._fieldratio
--- Snake/playingfield.py
+++ Snake/playingfield.py
@@ -1,4 +1,5 @@
 import pygame
+from const import Const
 
 
 class PlayingField:
@@ -7,8 +8,34 @@ class PlayingField:
             err = f'PLAYING FIELD: "display" should have class pygame.Surface,'
             err += f' but it is {type(display)}'
             raise TypeError(err)
-        self._display = display
+        self._const = Const()
+        self._display = None
+        self._width = 0
+        self._height = 0
+        self._margin = 0
+        self._fieldrect = ()
+        # self._menurect = ()
+        self.changeDisplay(display)
         pass
 
+    def changeDisplay(self, display):
+        if not isinstance(display, pygame.Surface):
+            err = f'PLAYING FIELD: "display" should have class pygame.Surface,'
+            err += f' but it is {type(display)}'
+            raise TypeError(err)
+        self._display = display
+        self._width = display.get_width()
+        self._height = display.get_height()
+        self._margin = min(int(self._width * self._const.MARGINRATIO),
+                           int(self._height * self._const.MARGINRATIO))
+        # self._menurect = ()
+
     def update(self):
+        pygame.draw.rect(self._display, self._const.BORDERCOLOR,
+                         (0, 0, self._width, self._height),
+                         self._margin)
+        x = int(self._width * self._const.MENURATIO)
+
+        pygame.draw.line(self._display, self._const.BORDERCOLOR, (x, 0),
+                         (x, self._height), self._margin)
         pass
